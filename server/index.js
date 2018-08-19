d5522aa3a53ef67e2ecc464b63a27587cae8360e
Jonathan Jacobs <jonathanMSJ@Hotmail.com>
2018-08-19 18:23:04 -0500

    Add endpoint for summit data

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
-const serve = require('koa-static');
 const Koa = require('koa');
+const serve = require('koa-static');
+const { get } = require('koa-route');
 
 const app = new Koa();
 
-// $ GET /hello.txt
+// JSON endpoint, adapted from https://github.com/koajs/route
+app.use(
+  get('/api/v1/peaks', ctx => {
+    const data = require('../src/data/summits.json');
+    ctx.body = data;
+  })
+);
+
 app.use(serve('src/public'));
 
 app.listen(3000);
-
+console.log('listening on port 3000');
 // -> Follow instructions at https://github.com/koajs/static
