117719e7a398b0fd382dc5ba5cd027084a4ac138
Drew Powers <drew@pow.rs>
2018-08-28 22:15:34 -0600

    Secure env vars

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
+const path = require('path');
 const Koa = require('koa');
 const serve = require('koa-static');
+const render = require('koa-ejs');
 const { get } = require('koa-route');
+const dotenv = require('dotenv');
+
+// Place this first
+dotenv.config();
 
 const app = new Koa();
+
+const ROOT_DIR = path.resolve(__dirname, '..', 'src', 'public');
+
+render(app, {
+  root: ROOT_DIR,
+  layout: false,
+  viewExt: 'ejs',
+});
 
 // JSON endpoint, adapted from https://github.com/koajs/route
 app.use(
@@ -13,7 +27,14 @@ app.use(
   })
 );
 
-app.use(serve('src/public'));
+app.use(serve(ROOT_DIR));
+
+app.use(ctx =>
+  ctx.render('index', {
+    // Pass server values to the template here
+    GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+  })
+);
 
 app.listen(3000);
 // -> Follow instructions at https://github.com/koajs/static
